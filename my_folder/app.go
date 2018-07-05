905c7d8d3e8af687ab962f6038e6a5a21e490657
pes-ca <28670923+Pesca23@users.noreply.github.com>
2018-07-05 23:31:16 +0900

    modified norikae

--- my_folder/app.go
+++ my_folder/app.go
@@ -128,8 +128,10 @@ func handleNorikae(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprint(w, network[0])
 	// -> {Outer Loop [Pallet Town Viridian City Pewter City Cerulean City Lavender Town Fuschia City Celadon City Saffron City Lavender Town]}
 
-	fmt.Fprint(w, network[0].Stations)
+	// fmt.Fprint(w, network[0].Stations)
+	// [Pallet Town Viridian City Pewter City Cerulean City Lavender Town Fuschia City Celadon City Saffron City Lavender Town]
 
+	fmt.Fprint(w, network[0].Stations[0])
 
 
 	// handleExampleと同じようにtemplateにテンプレートを埋めて、出力する。
