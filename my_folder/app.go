3d0ca556aac7dc95d776a18574172ea92079b79c
pes-ca <28670923+Pesca23@users.noreply.github.com>
2018-07-06 11:37:33 +0900

    modified norikae

--- my_folder/app.go
+++ my_folder/app.go
@@ -157,6 +157,7 @@ func networkInterpreter(w http.ResponseWriter, network TransitNetwork) {
 	// ...
 
 	cityToNum := make(map[string]int)
+	// map[]
 	// fmt.Fprint(w, cityToNum)
 
 	numToCity := make([]string, 0)
@@ -164,15 +165,17 @@ func networkInterpreter(w http.ResponseWriter, network TransitNetwork) {
 	cityToLoops := make([]Dic, 0)
 	// loopToCities := make([]Dic, 0)
 
-	l := new(Dic)
-	fmt.Fprint(w, l)
+	//l := new(Dic)
+	// &{0 []}
+	//fmt.Fprint(w, l)
 
 
 	// i < len(network)
 	for i := 0; i < 1 ; i++ {
 		numToLoop = append(numToLoop, network[i].Name)
+		fmt.Fprint(w, numToLoop)
+		
 
-		//fmt.Fprint(w, numToLoop)
 		// j < len(network[i].Stations
 		for j := 0; j < 1 ; j++ {
 			// city: network[i].Stations[j]
@@ -182,7 +185,8 @@ func networkInterpreter(w http.ResponseWriter, network TransitNetwork) {
     	} else {
 				numToCity = append(numToCity, network[i].Stations[j])
 				cityToNum[network[i].Stations[j]] = len(numToCity)-1
-				// fmt.Fprint(w, l)
+
+
 				// append(cityToLoops, {len(numToCity)-1 [len(numToLoop)-1]})
     	}
 		}
