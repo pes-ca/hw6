87d4a336abd0b60ef5511e11db6c8a9b3821d778
pes-ca <28670923+Pesca23@users.noreply.github.com>
2018-07-06 12:56:11 +0900

    modified norikae

--- my_folder/app.go
+++ my_folder/app.go
@@ -143,8 +143,8 @@ func handleNorikae(w http.ResponseWriter, r *http.Request) {
 }
 
 func existOrNot(cities [][]int, last int, current int) int, int {
-	flag1 := 0
-	flag2 := 0
+	var flag1 = 0
+	var flag2 = 0
 
 	for n := 0; n < len(cities[last]) ; n ++ {
 		if cities[last][n] == current {
