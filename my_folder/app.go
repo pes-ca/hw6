73b834f7b5596b184d1a2c6eaed71e9c55652fac
pes-ca <28670923+Pesca23@users.noreply.github.com>
2018-07-06 12:04:03 +0900

    modified norikae

--- my_folder/app.go
+++ my_folder/app.go
@@ -177,7 +177,8 @@ func networkInterpreter(w http.ResponseWriter, network TransitNetwork) {
 
 			// if the city has been seen before
 			if cityID, ok := cityToNum[network[i].Stations[j]]; ok {
-        cityToLoops[cityID].loops = append(cityToLoops[cityID].loops, len(numToLoop)-1)
+        // cityToLoops[cityID].loops = append(cityToLoops[cityID].loops, len(numToLoop)-1)
+				fmt.Fprint(w, cityID, "\n")
 				fmt.Fprint(w, "cityToLoops:", cityToLoops, "\n")
     	} else {
 				numToCity = append(numToCity, network[i].Stations[j])
